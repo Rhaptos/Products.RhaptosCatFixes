df5bad726a58a0029494d3d72ee2e67e52beb37b
Ross J. Reedstrom <reedstrm@cnx.org>
2011-05-03 21:30:32 +0000

    make external utility meothds importable, and generalize to additional cat fixes

--- Products/RhaptosCatFixes/Extensions/catalogUtils.py
+++ Products/RhaptosCatFixes/Extensions/catalogUtils.py
@@ -3,29 +3,52 @@ from StringIO import StringIO
 def checkCatalog(self):
     out = StringIO()
     catalog = self.portal_catalog._catalog
-
+    idx = catalog.schema['portal_type']
     stale = 0
     pfactory = 0
-    for uid in catalog.uids.keys():
-	if 'portal_factory' in uid:
-	    pfactory += 1
-	    print >> out, uid
-	else:
-	    try:
-	        ob = self.unrestrictedTraverse(uid)
-	    except Exception, msg:
-	        print >>oud, msg
-	        stale += 1
-
-    print >> out, 'total: ', len(catalog.uids)
-    print >> out, 'portal_factory: ', pfactory
-    print >> out, 'stale: ', stale
+    pcp = 0
+    cdoc = 0
+    for uid, rid in catalog.uids.items():
+        val = catalog.data[rid][idx]
+        if val == 'PublishedContentPointer':
+            pcp += 1
+#            print >> out, uid
+        elif val == 'CNXML Document':
+            cdoc += 1
+#            print >> out, uid
+        elif 'portal_factory' in uid:
+            pfactory += 1
+#            print >> out, uid
+        else:
+            try:
+                ob = self.unrestrictedTraverse(uid)
+            except Exception, msg:
+                print >>out, msg
+                stale += 1
+    print >> out, '# indexes: ', len(catalog.indexes)
+    print >> out, 'total objects: ', len(catalog.uids)
+    print >> out, '  PublishedContentPointer: ', pcp
+    print >> out, '  CNXML Document: ', cdoc
+    print >> out, '  portal_factory: ', pfactory
+    print >> out, '  stale: ', stale
     return out.getvalue()
 
 
 def modifyCatalog(self):
     out = StringIO()
-    catalog = self.portal_catalog._catalog
+
+    cat = self.portal_catalog
+    existing_indexes = cat.indexes()
+    keep_indexes = ['Creator', 'portal_type', 'allowedRolesAndUsers', 'orig_id', 'review_state',
+                    'path', 'getObjPositionInParent', 'sortable_title', 'modified', 'created',
+                    'Date', 'effectiveRange']
+
+    for i in keep_indexes:
+        existing_indexes.remove(i)
+
+    cat.manage_delIndex(existing_indexes)
+
+    catalog = cat._catalog
     idx = catalog.schema['portal_type']
 
     print >> out, 'total: ', len(catalog.uids)
@@ -33,13 +56,13 @@ def modifyCatalog(self):
     content = []
     for uid, rid in catalog.uids.items():
         val = catalog.data[rid][idx]
-        if val == 'PublishedContentPointer':
-    	    content.append(uid)
-    	elif 'portal_factory' in uid:
-    	    content.append(uid)
+        if val in  ['CNXML Document','PublishedContentPointer']:
+            content.append(uid)
+        elif 'portal_factory' in uid:
+            content.append(uid)
     
     for uid in content:
-	catalog.uncatalogObject(uid)
+        catalog.uncatalogObject(uid)
 
     print >> out, len(content)
     return out.getvalue()
